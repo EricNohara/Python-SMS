f4087292ec988541ea5c55b0f78b18e0f906b92f
ericnoharaleclair <eric.noharaleclair@gmail.com>
2024-01-16 15:43:48 -0500

    added function to send mms type messages and added it to the main function

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from sms import send_sms_by_email
+from mms import send_mms_by_email
 
 def main():
     number = "6363179533"
     message = "Testing this script"
     provider = "AT&T"
     sender_credentials = ("eric.noharaleclair@gmail.com", "ujct wmrm kbjl pgun")
-    send_sms_by_email(number, message, provider, sender_credentials)
+    # send_sms_by_email(number, message, provider, sender_credentials)
+
+    # SENDING MMS
+    file_path = "sus.png"
+    mime_maintype = "image"
+    mime_subtype = "png"
+
+    send_mms_by_email(number, message, file_path, mime_maintype, mime_subtype, provider, sender_credentials)
 
 if __name__ == "__main__":
     main()
