14052a52bf24f97d03892e0cf1ed97ad6530b1fd
ericnoharaleclair <eric.noharaleclair@gmail.com>
2024-01-16 14:57:12 -0500

    initialized function

--- sms.py
+++ sms.py
@@ -1,2 +1,12 @@
 import email, smtplib, ssl
 from providers import PROVIDERS
+
+def send_sms_by_email (
+        number: str, 
+        essage: str, 
+        provider: str, 
+        sender_credentials: tuple, 
+        subject: str="sent from python", 
+        smtp_server: str ="smtp.gmail.com", 
+        smtp_port: int = 465):
+    pass
