37d15deeb18ae0ddc47b6bf932916d65cc0614e2
ericnoharaleclair <eric.noharaleclair@gmail.com>
2024-01-18 00:56:08 -0500

    added initial testing code for pandas getting cells from google sheets, and also for finding the current day in the workout cycle using the date library

--- getsheetdata.py
+++ getsheetdata.py
@@ -1,18 +1,36 @@
 import getsheet
+from datetime import date
+
+START_DATE = date(2023, 12, 17)
+today = date.today()
+
+total_days_since = (today - START_DATE).days
+weeks_since = total_days_since // 7
+# WEEK AND DAY NUM IN CURRENT CYCLE
+week_num = weeks_since + 1
+day_num = total_days_since % 7
+
+print(f"Week {week_num}, Day {day_num}")
+
+day_num_to_lift_num = {
+    1: 1,
+    2: -1,
+    3: 2,
+    4: -1,
+    5: 3,
+    6: 4,
+    7: -1
+}
+
+print(day_num_to_lift_num[day_num])
+
+
 
 url = 'https://docs.google.com/spreadsheets/d/1fMjyr4TgCrfVDT1U8fkXCfOyxMf6ulrWpuhltlKi974/edit#gid=0'
 
-week = 3
-day = "Monday"
+cur_sheet = getsheet.get_sheet_from_url(url)
 
-lifting_days = {
-    "Monday": 1,
-    "Tuesday": -1,
-    "Wednesday": 2,
-    "Thursday": -1,
-    "Friday": 3,
-    "Saturday": 4,
-    "Sunday": -1
-}
+print(cur_sheet.loc[0:5])
+    
 
-print(getsheet.get_sheet_from_url(url))
+
